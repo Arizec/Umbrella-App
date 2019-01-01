a0bcc1f2a416ba9e9afa6267bf8e9e5c5da77819
Gabrielle Chan <salufie@gmail.com>
2019-01-01 17:17:03 +1100

    renamed the application

---
+++ Will It Rain Today/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Will It Rain Today
+//
+//  Created by Gabrielle Chandrasaputra on 1/1/19.
+//  Copyright © 2019 Arizec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
