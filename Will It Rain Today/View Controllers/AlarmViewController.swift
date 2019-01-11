601c121dc95d107e87d70daa475ad46cc6500132
Unknown <salufie@gmail.com>
2019-01-11 15:02:22 +1100

    implemented CoreData to save notification time

--- Will It Rain Today/View Controllers/AlarmViewController.swift
+++ Will It Rain Today/View Controllers/AlarmViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import UserNotifications
+import CoreData
 
 class AlarmViewController: UIViewController, UNUserNotificationCenterDelegate {
     var rainToday = ""
@@ -15,9 +16,44 @@ class AlarmViewController: UIViewController, UNUserNotificationCenterDelegate {
     @IBOutlet weak var datePicker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        fetchData()
 
-        // Do any additional setup after loading the view.
-        print(rainToday)
+
+    }
+    
+    func fetchData(){
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        let context = delegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName:"Time")
+        request.returnsObjectsAsFaults = false
+        
+        do{
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+                let minute = data.value(forKey: "minute") as! Int
+                let hour = data.value(forKey: "hour") as! Int
+                
+                
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat =  "HH:mm"
+                
+                let date = dateFormatter.date(from: "\(hour):\(minute)")
+                
+                datePicker.date = date!
+
+                
+            }
+            
+            
+        }
+        catch{}
+
     }
     
 
@@ -32,12 +68,44 @@ class AlarmViewController: UIViewController, UNUserNotificationCenterDelegate {
         
     }
     
+    func deleteAllRecords() {
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        let context = delegate.persistentContainer.viewContext
+        
+        let deleteFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Time")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: deleteFetch)
+        
+        do {
+            try context.execute(deleteRequest)
+            try context.save()
+        } catch {
+            print ("There was an error")
+        }
+    }
+    
     
     @IBAction func getTime(_ sender: Any) {
         // get date from datepicker
         let calendar = Calendar.current
         var components = calendar.dateComponents([.second, .hour, .minute], from: self.datePicker.date)
         components.second = 0
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let time = NSEntityDescription.insertNewObject(forEntityName: "Time", into: context)
+        
+        deleteAllRecords()
+        
+        time.setValue(components.minute, forKey: "minute")
+        time.setValue(components.hour, forKey: "hour")
+        
+        fetchData()
+        
+        do{
+            try context.save()
+            print("SAVED")
+        }
+        catch{}
         
         // Will it rain today
         let sendNotif = Array(rainToday)[0] == "N" ? false : true
